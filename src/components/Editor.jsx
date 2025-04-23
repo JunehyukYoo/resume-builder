0acf255dc909df475ca21e82b75e92b72880f305
Junehyuk Yoo <notjune@DESKTOP-5OM7J7U.>
2025-04-23 14:32:13 -0500

    Basic outline of project.

---
+++ src/components/Editor.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const Editor = () => {
+  return (
+    <div className="editor">
+      <h1>Editor Component</h1>
+      {/* Add your editor functionality here */}
+      <p>This is where the editor will be displayed.</p>
+    </div>
+  );
+};
+
+export default Editor;
---
+++ src/components/Preview.jsx
@@ -0,0 +1,13 @@
+function Preview() {
+  return (
+    <>
+      <div className="preview">
+        <h1>Preview Component</h1>
+        {/* Add your preview functionality here */}
+        <p>This is where the preview will be displayed.</p>
+      </div>
+    </>
+  );
+}
+
+export default Preview;
